8176c8c88fae54378bb90ee81f2304cca0882cb4
Symi <symi.contact@gmail.com>
2020-12-05 20:59:18 +0100

    Metrics update

--- src/main/java/me/symi/chat/FabledChat.java
+++ src/main/java/me/symi/chat/FabledChat.java
@@ -1,13 +1,46 @@
 package me.symi.chat;
 
+import me.symi.chat.metrics.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FabledChat extends JavaPlugin {
 
     @Override
     public void onEnable()
     {
+        setupMetrics();
+    }
 
+    private void setupMetrics()
+    {
+        Metrics metrics = new Metrics(this, 9566);
+        metrics.addCustomChart(new Metrics.DrilldownPie("java_version", () ->
+        {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+            String javaVersion = System.getProperty("java.version");
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(javaVersion, 1);
+            if (javaVersion.startsWith("1.7"))
+            {
+                map.put("Java 1.7", entry);
+            }
+            else if (javaVersion.startsWith("1.8"))
+            {
+                map.put("Java 1.8", entry);
+            }
+            else if (javaVersion.startsWith("1.9"))
+            {
+                map.put("Java 1.9", entry);
+            }
+            else
+            {
+                map.put("Other", entry);
+            }
+            return map;
+        }));
     }
 
 }
--- src/main/java/me/symi/chat/utils/ChatUtil.java
+++ src/main/java/me/symi/chat/utils/ChatUtil.java
@@ -1,5 +1,23 @@
 package me.symi.chat.utils;
 
+import org.bukkit.ChatColor;
+
+import java.util.List;
+
 public class ChatUtil {
+
+    public static String fixColors(final String text)
+    {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    public static List<String> fixColors(final List<String> list)
+    {
+        for(int i = 0; i < list.size(); i++)
+        {
+            list.set(i, fixColors(list.get(i)));
+        }
+        return list;
+    }
 
 }
